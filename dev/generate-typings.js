73e484758944fca4d9c1d06625d47c2ab6acb2ee
Jussi Räsänen <jussi@protonmail.com>
2022-05-07 13:13:45 +0300

    chore: generate typescript types

---
+++ dev/generate-typings.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const camelCase = require('lodash.camelcase');
+
+const commands = require('../src/commands.js');
+
+console.log("declare class Client {")
+console.log("  constructor(options);")
+Object.entries(commands)
+    .forEach(
+        ([name, {arguments = []}]) => {
+            const fnCall = camelCase(name)
+            const args = arguments.map(x => `${x}: string`).join(', ')
+            console.log(`  ${fnCall}(${args}): Promise<any>;`)
+        }
+    )
+console.log("}")
